f4c87afca9b3b22e8b709b0b5e03cb76705696f0
shivshanar <chaudharyshivshanarex09@gmail.com>
2024-03-01 19:07:43 -0800

    second commit

--- HMS/HMS/settings.py
+++ HMS/HMS/settings.py
@@ -124,4 +124,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-# AUTH_USER_MODEL= hotel.User
+# AUTH_USER_MODEL= hotel.CustomUser
--- HMS/hotel/views.py
+++ HMS/hotel/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render,HttpResponse
+from django.views.generic import ListView,FormView
 from .models import Room, Booking
+from .forms import AvailabilityForm
+from hotel.booking_functions.availability import check_availability
+
 
 
 class RoomList(ListView):
@@ -8,3 +11,32 @@ class RoomList(ListView):
     
 class BookingList(ListView):
     model = Booking
+    
+class BookingView(FormView):
+    form_class = AvailabilityForm
+    template_name = 'hotel/availability_form.html'
+    
+    def form_valid(self, form):
+        data = form.cleaned_data
+        room_list = Room.objects.filter(category=data['room_category'])
+        available_rooms =[]
+        for room in room_list:
+            if check_availability(room,data['check_in'], data['check_out']):
+                available_rooms.append(room)
+        if len(available_rooms) >0:        
+           room = available_rooms[0] #1st room is available
+           booking = Booking.objects.create(
+            #    user = get_user_model(),
+               user = self.request.user,
+               room = room,
+               check_in= data['check_in'],
+               check_out = data['check_out']
+           )
+           booking.save()
+           return HttpResponse(booking)
+        else:
+            return HttpResponse('All of this category room are booked!! Try another one')
+            
+                
+    
+     
